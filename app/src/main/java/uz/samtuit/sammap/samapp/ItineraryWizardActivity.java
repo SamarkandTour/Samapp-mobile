14a95b5b25b8807eb05fd9ff0cbc66a84be64c10
alexb007 <alex960126@gmail.com>
2015-07-28 16:26:35 +0500

    28.07.2015

--- app/src/main/java/uz/samtuit/sammap/samapp/ItineraryWizardActivity.java
+++ app/src/main/java/uz/samtuit/sammap/samapp/ItineraryWizardActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -25,6 +26,7 @@ public class ItineraryWizardActivity extends Activity {
     Button btn;
     LinearLayout ll;
     FrameLayout fl;
+    TextView tv;
 
 
 
@@ -33,12 +35,18 @@ public class ItineraryWizardActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_wizard);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
+        tv = (TextView)findViewById(R.id.suggestedItineraryTitle);
+        tv.setTypeface(tf);
         ll = (LinearLayout)findViewById(R.id.tabButtonsLayout);
         for(int i = 0; i < daysCount; i++)
         {
             btn = new Button(this);
-            btn.setBackgroundResource(R.drawable.tab_button_back);
+            btn.setPadding(0,0,0,0);
+            btn.setTextSize(12);
             btn.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            btn.setText("Day " + (i+1));
+            btn.setTypeface(tf);
             ll.addView(btn);
         }
     }
