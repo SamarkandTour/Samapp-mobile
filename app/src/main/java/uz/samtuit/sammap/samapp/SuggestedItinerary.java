95eac3da960ce6600682210de1330697d7934f4c
alexb007 <alex960126@gmail.com>
2015-09-28 08:53:13 +0500

    new material design sug itinerary

--- app/src/main/java/uz/samtuit/sammap/samapp/SuggestedItinerary.java
+++ app/src/main/java/uz/samtuit/sammap/samapp/SuggestedItinerary.java
@@ -5,47 +5,39 @@ import android.app.TabActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TabHost;
-import android.widget.TabWidget;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
-import com.cocoahero.android.geojson.GeoJSON;
-import com.cocoahero.android.geojson.GeoJSONObject;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 
-public class SuggestedItinerary extends Activity {
+public class SuggestedItinerary extends ActionBarActivity {
 
     Button btn;
     LinearLayout ll;
     FrameLayout fl;
     TextView tv;
-    String day[] = {"one","two","three","five","six","seven","eight","nine","ten"};
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter vpadapter;
     ListView lv;
     Point[] dat;
     ArrayList<Point[]> data;
     SugItineraryAdapter adapter;
+    SlidingTabLayout tabs;
 
 
     private final static int daysCount = 2;
@@ -53,92 +45,32 @@ public class SuggestedItinerary extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_wizard);
-        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
-        tv = (TextView)findViewById(R.id.suggestedItineraryTitle);
-        tv.setTypeface(tf);
-        try
-        {
-
-        }
-        catch(Exception e){
-
-        }
-        genData();
-
-        lv = (ListView)findViewById(R.id.listView);
-        ll = (LinearLayout)findViewById(R.id.tabButtonsLayout);
-        adapter = new SugItineraryAdapter(this, R.layout.itinerary_list_item, data.get(0));
-        lv.setAdapter(adapter);
+        CharSequence Titles[] = new CharSequence[daysCount];
         for(int i = 0; i < daysCount; i++)
-        {
-            btn = new Button(this);
-            btn.setPadding(0, 0, 0, 0);
-            btn.setTextSize(12);
-            btn.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            btn.setText("Day " + (i + 1));
-            btn.setTypeface(tf);
-            btn.setOnClickListener(new Button.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.e("ASD", Integer.parseInt(((Button) view).getText().toString().split(" ")[1]) - 1 + " ");
-                    changeAdapter(Integer.parseInt(((Button) view).getText().toString().split(" ")[1]) - 1);
-                    //lv.setAdapter(adapters.get(Integer.parseInt(((Button)view).getText().toString().split(" ")[1])-1));
-                }
-            });
-            ll.addView(btn);
-        }
+            Titles[i] = "Day "+(i+1);
+        toolbar = (Toolbar)findViewById(R.id.si_toolbar);
+        toolbar.setTitle("Suggested Itinerary");
+        toolbar.setBackgroundColor(getResources().getColor(R.color.ColorPrimary));
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        setSupportActionBar(toolbar);
+        vpadapter = new ViewPagerAdapter(getSupportFragmentManager(),Titles,daysCount);
 
-        //Json Settings
+        pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(vpadapter);
 
-    }
-    private String getItemName(int i)
-    {
-        return "day_" + day[i];
-    }
+        tabs = (SlidingTabLayout)findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true);
 
-    private void changeAdapter(int i)
-    {
-        adapter = new SugItineraryAdapter(this,R.layout.itinerary_list_item,data.get(i));
-        lv.setAdapter(adapter);
-    }
-    private void genData()
-    {
-        data = new ArrayList<Point[]>();
-        dat = new Point[4];
-        dat[0]= new Point("Registan","39.65219, 66.94716","ADAS");
-        dat[1]= new Point("Guri Emir","39.65552, 66.95680","ADAS");
-        dat[2]= new Point("Rukhabad","39.66166, 66.97904","ADAS");
-        dat[3]= new Point("Afrasiab Hotel","39.66607, 66.97932","ADAS");
-        data.add(dat);
-        dat = new Point[3];
-        dat[2]= new Point("Siyob Bazar","39.65219, 66.94716","ADAS");
-        dat[1]= new Point("Afrasiab","39.65552, 66.95680","ADAS");
-        dat[0]= new Point("Shakhizinda","39.66166, 66.97904","ADAS");
-        data.add(dat);
-    }
-
-    public String loadJSONFromAsset(String fileName) {
-        String json = null;
-        try {
-
-            InputStream is = getAssets().open(fileName);
-
-            int size = is.available();
-
-            byte[] buffer = new byte[size];
-
-            is.read(buffer);
-
-            is.close();
-
-            json = new String(buffer, "UTF-8");
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.selector);
+            }
+        });
+        tabs.setViewPager(pager);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
 
 
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
     }
 
 }
